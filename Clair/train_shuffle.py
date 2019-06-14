3c19a704f41ee7cb1a1dbae21b4bc54f4ef6f238
kayik <>
2019-06-14 10:57:02 +0800

    using for loop on the same batch to test the decay of the learning rate

--- Clair/train_shuffle.py
+++ Clair/train_shuffle.py
@@ -74,7 +74,7 @@ def shuffle_first_n_items(array, n):
     return np.append(a1, a2)
 
 
-def new_mini_batch(data_index, validation_data_start_index, dataset_info, tensor_block_index_list,global_step):
+def new_mini_batch(data_index, validation_data_start_index, dataset_info, tensor_block_index_list):
     dataset_size = dataset_info["dataset_size"]
     x_array_compressed = dataset_info["x_array_compressed"]
     y_array_compressed = dataset_info["y_array_compressed"]
@@ -82,7 +82,7 @@ def new_mini_batch(data_index, validation_data_start_index, dataset_info, tensor
     validation_batch_size = param.predictBatchSize
 
     if data_index >= dataset_size:
-        return None, None, 0,0
+        return None, None, 0
 
     # calculate new batch size according to dataset index
     # train: 0 - validation_data_start_index - 1, validation: validation_data_start_index - dataset_size
@@ -103,11 +103,10 @@ def new_mini_batch(data_index, validation_data_start_index, dataset_info, tensor
         x_array_compressed, data_index, batch_size, dataset_size, tensor_block_index_list)
     y_batch, y_num, y_end_flag = utils.decompress_array_with_order(
         y_array_compressed, data_index, batch_size, dataset_size, tensor_block_index_list)
-    global_step +=1
     if x_num != y_num or x_end_flag != y_end_flag:
         sys.exit("Inconsistency between decompressed arrays: %d/%d" % (x_num, y_num))
 
-    return x_batch, y_batch, x_num, global_step
+    return x_batch, y_batch, x_num
 
 
 def train_model(m, training_config):
@@ -141,8 +140,7 @@ def train_model(m, training_config):
     #learning_rate_switch_count = param.maxLearningRateSwitch
     validation_start_block = int(validation_data_start_index / param.bloscBlockSize) - 1
     decay_step=int(no_of_training_examples/param.trainBatchSize)
-    global_step=0
-
+    global_step=list(range(0,decay_step))
 
     # Initialize variables
     epoch_count = 1
@@ -171,19 +169,19 @@ def train_model(m, training_config):
         # threads for either train or validation
         thread_pool = []
         if is_with_batch_data and is_training:
-            thread_pool.append(Thread(target=m.train, args=(x_batch, y_batch,global_step,decay_step,m.decay_learning_rate, True)))
+            for step in global_step:
+                thread_pool.append(Thread(target=m.train, args=(x_batch, y_batch,step,decay_step,m.decay_learning_rate, True)))
         elif is_with_batch_data and is_validation:
             thread_pool.append(Thread(target=m.get_loss, args=(x_batch, y_batch, True)))
         for t in thread_pool:
             t.start()
 
 
-        next_x_batch, next_y_batch, batch_size,next_global_step= new_mini_batch(
+        next_x_batch, next_y_batch, batch_size= new_mini_batch(
             data_index=data_index,
             validation_data_start_index=validation_data_start_index,
             dataset_info=dataset_info,
             tensor_block_index_list=tensor_block_index_list,
-            global_step=global_step
         )
 
         # wait until loaded next mini batch & finished training/validation with current mini batch
@@ -211,11 +209,10 @@ def train_model(m, training_config):
         data_index += batch_size
 
         # if not go through whole dataset yet (have next x_batch and y_batch data), continue the process
-        if next_x_batch is not None and next_y_batch is not None:
-            x_batch = next_x_batch
-            y_batch = next_y_batch
-            global_step = next_global_step
-            continue
+        #if next_x_batch is not None and next_y_batch is not None:
+        #    x_batch = next_x_batch
+        #    y_batch = next_y_batch
+        #    continue
 
         logging.info(
             " ".join([str(epoch_count), "Training loss:", str(training_loss_sum/no_of_training_examples)])
@@ -275,7 +272,6 @@ def train_model(m, training_config):
 
         epoch_start_time = time.time()
         learning_rate=param.initialLearningRate
-        global_step=0
         training_loss_sum = 0
         validation_loss_sum = 0
         data_index = 0
