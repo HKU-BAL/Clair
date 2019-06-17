aa81539b89f3c420b3a2bd9f17c162fcf87a3720
kayik <>
2019-06-17 11:38:52 +0800

    fine-tune for the previously updated version

--- Clair/train_shuffle.py
+++ Clair/train_shuffle.py
@@ -279,6 +279,7 @@ def train_model(m, training_config):
         data_index = 0
         x_batch = None
         y_batch = None
+        global_step=0
 
         base_change_loss_sum = 0
         genotype_loss_sum = 0
