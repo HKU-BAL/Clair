e343c6a0062b3e1ea73f7d920448a2b9fdc49786
kayik <>
2019-08-14 16:29:48 +0800

    bug fixed

--- Clair/callVarBamParallel.py
+++ Clair/callVarBamParallel.py
@@ -160,14 +160,21 @@ def Run(args):
                     CommandOption('ctgEnd', end),
                     CommandOption('call_fn', output_fn)
                 ]
-                if is_bed_file_provided and chromName in tree and len(tree[chromName].search(start, end)) != 0:
-                    additional_options.append(CommandOption('bed_fn', bed_fn))
-
-                if args.activation_only:
-                    print(command_string + " " + executable_command_string_from(additional_options+activation_only_commands))
+                if is_bed_file_provided:
+                    if chromName in tree and len(tree[chromName].search(start, end)) != 0:
+                        additional_options.append(CommandOption('bed_fn', bed_fn))
+                        if args.activation_only:
+                            print(command_string + " " + executable_command_string_from(additional_options+activation_only_commands))
+                        else:
+                            print(command_string + " " + executable_command_string_from(
+                                        additional_options))
                 else:
-                    print(command_string + " " + executable_command_string_from(
-                                additional_options))
+                    if args.activation_only:
+                        print(command_string + " " + executable_command_string_from(
+                            additional_options + activation_only_commands))
+                    else:
+                        print(command_string + " " + executable_command_string_from(
+                            additional_options))
 
 
 if __name__ == "__main__":
